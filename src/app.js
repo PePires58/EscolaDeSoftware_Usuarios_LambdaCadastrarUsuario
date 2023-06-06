2d0ede5b9b0cb9c58cd13b0cff48d241453f1148
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-06 11:34:43 -0300

    adicionando log

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const putItemDynamoDbService = require('./services/put-item-dynamodb.service');
 
 exports.lambdaHandler = async (event, context) => {
 
+    console.log(event);
+
     const errors = validateUserObjectService.validateUserObject(event.body);
     if (errors.length > 0)
         return errorResult(400, errors);
